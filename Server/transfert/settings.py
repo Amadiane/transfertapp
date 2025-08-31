7552b93afaf95a96a91531fdfa4abc0056ecc98b
Amadiane <129948408+Amadiane@users.noreply.github.com>
2025-08-31 05:44:56 +0100

    jj

--- Server/transfert/settings.py
+++ Server/transfert/settings.py
@@ -1,45 +1,33 @@
 from pathlib import Path
 from datetime import timedelta
-
 import os
-from urllib.parse import urlparse 
-from django.core.management.utils import get_random_secret_key 
-import sys 
+from django.core.management.utils import get_random_secret_key
 
-
-
-
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-nq*6_twxlu-^)^!re(2ununr(46=rr$9ab6g(4egcs_fd_k&*@'
+# ------------------------
+# Sécurité
+# ------------------------
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
-
-# SECURITY WARNING: don't run with debug turned on in production!
-
-# DEBUG = os.getenv("DEBUG", "False") == "True"
 DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "102.164.134.4"]
-
-
-# DEBUG = False
-# ALLOWED_HOSTS = [os.getenv("DJANGO_ALLOWED_HOSTS", "diallodiallotransfertapp.ondigitalocean.app")]
-
-# ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "127.0.0.1, localhost").split(",")
+ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
 
 AUTH_USER_MODEL = 'Base.User'
 
+# ------------------------
+# CORS
+# ------------------------
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # l'URL de ton frontend
+    "http://localhost:3000",  # l'URL de ton frontend React
 ]
 
-# Application definition
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+]
 
+# ------------------------
+# Apps
+# ------------------------
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -64,7 +52,6 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-
 ROOT_URLCONF = 'transfert.urls'
 
 TEMPLATES_DIR = BASE_DIR.parent / "Administrator" / "build"
@@ -85,7 +72,9 @@ TEMPLATES = [
     },
 ]
 
-
+# ------------------------
+# REST & JWT
+# ------------------------
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
@@ -95,122 +84,60 @@ REST_FRAMEWORK = {
     ),
 }
 
-
-CSRF_TRUSTED_ORIGINS = ['http://localhost:3000']
-
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
-    'BLACKLIST_AFTER_ROTATION': True,  # important
+    'BLACKLIST_AFTER_ROTATION': True,
     'ROTATE_REFRESH_TOKENS': True,
     'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
     'TOKEN_BLACKLIST_ENABLED': True,
 }
 
-
 WSGI_APPLICATION = 'transfert.wsgi.application'
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',  # Le fichier SQLite sera créé ici
-#     }
-# }
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'transfertdatabase',
-#         'USER': 'root',
-#         'PASSWORD': '',
-#         'HOST': 'localhost',  # ou l'IP du serveur MySQL
-#         'PORT': '3308',
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-#         }
-#     }
-# }
-
-import os
-
-
-DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "True") == "True"
-
-if DEVELOPMENT_MODE:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'transfertdatabase',
-            'USER': 'ama',
-            'PASSWORD': 'ama',
-            'HOST': '127.0.0.1',  # localhost peut parfois poser problème avec MySQL + auth_socket
-            'PORT': '3306',
-            'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
+# ------------------------
+# Base de données (locale MySQL)
+# ------------------------
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'transfertdatabase',
+        'USER': 'ama',
+        'PASSWORD': 'ama',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
         }
     }
-else:
-    # Base production sur DigitalOcean MySQL
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': os.getenv("DB_NAME"),
-            'USER': os.getenv("DB_USER"),
-            'PASSWORD': os.getenv("DB_PASSWORD"),
-            'HOST': os.getenv("DB_HOST"),
-            'PORT': os.getenv("DB_PORT", "3306"),
-            'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"}
-        }
-    }
+}
 
-
-
+# ------------------------
 # Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
+# ------------------------
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
-
+# ------------------------
+# Internationalisation
+# ------------------------
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
+# ------------------------
+# Static files
+# ------------------------
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
-# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     BASE_DIR / "Administrator" / "build" / "static",
 ]
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
