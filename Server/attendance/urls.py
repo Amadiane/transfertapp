070639dcbf02e3081ef58f7c148bb0c827ea19c2
Amadiane <129948408+Amadiane@users.noreply.github.com>
2025-09-02 02:55:35 +0100

    jj

--- Server/attendance/urls.py
+++ Server/attendance/urls.py
@@ -1,6 +1,19 @@
-from . import views 
-from django.conf import settings 
-from django.urls import path, include 
+# from . import views 
+# from django.conf import settings 
+# from django.urls import path, include 
+# from django.conf.urls.static import static
+
+# urlpatterns = [ path('', views.index, name='index'), ]
+from . import views
+from django.urls import re_path
+from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [ path('', views.index, name='index'), ]
+urlpatterns = [
+    # toutes les routes non gérées par Django vont vers React
+    re_path(r'^.*$', views.index, name='index'),
+]
+
+# si tu as des fichiers statiques à servir en développement
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Server/attendance/views.py
+++ Server/attendance/views.py
@@ -1,13 +1,32 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+# # from django.shortcuts import render
+# from django.http import HttpResponse
+# import os
+
+# def index(request):
+#     # Chemin correct vers le build frontend
+#     frontend_build_path = os.path.join(os.path.dirname(__file__), "..", "..", "Administrator", "build", "index.html")
+    
+#     if not os.path.exists(frontend_build_path):
+#         return HttpResponse("Fichier index.html introuvable.", status=404)
+    
+#     with open(frontend_build_path, "r", encoding="utf-8") as f:
+#         return HttpResponse(f.read())
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
 import os
 
 def index(request):
-    # Chemin correct vers le build frontend
-    frontend_build_path = os.path.join(os.path.dirname(__file__), "..", "..", "Administrator", "build", "index.html")
-    
-    if not os.path.exists(frontend_build_path):
-        return HttpResponse("Fichier index.html introuvable.", status=404)
-    
-    with open(frontend_build_path, "r", encoding="utf-8") as f:
-        return HttpResponse(f.read())
+    """
+    Sert le fichier index.html de React pour toutes les routes,
+    afin que React Router gère le routage côté client.
+    """
+    # Chemin absolu vers le build React
+    path_to_index = os.path.join(
+        os.path.dirname(__file__), "..", "..", "Administrator", "build", "index.html"
+    )
+
+    if os.path.exists(path_to_index):
+        with open(path_to_index, 'r', encoding='utf-8') as f:
+            return HttpResponse(f.read())
+    else:
+        return HttpResponseNotFound("index.html introuvable. Vérifie le chemin de build React.")
