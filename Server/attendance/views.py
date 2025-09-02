0123af6deaa0ad058bf84e245464b4dc4cdb2524
Amadiane <129948408+Amadiane@users.noreply.github.com>
2025-09-02 02:35:45 +0100

    jj

--- Server/attendance/views.py
+++ Server/attendance/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 from django.http import HttpResponse
 import os
 
 def index(request):
-    with open(os.path.join(os.path.dirname(__file__), "..", "Administrator", "build", "index.html")) as f:
+    # Chemin correct vers le build frontend
+    frontend_build_path = os.path.join(os.path.dirname(__file__), "..", "..", "Administrator", "build", "index.html")
+    
+    if not os.path.exists(frontend_build_path):
+        return HttpResponse("Fichier index.html introuvable.", status=404)
+    
+    with open(frontend_build_path, "r", encoding="utf-8") as f:
         return HttpResponse(f.read())
