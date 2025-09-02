c379571702bd145b1ef11769e6ba16a9e27ad1fb
Amadiane <129948408+Amadiane@users.noreply.github.com>
2025-09-02 03:02:47 +0100

    jj

--- Server/transfert/settings.py
+++ Server/transfert/settings.py
@@ -19,6 +19,7 @@ AUTH_USER_MODEL = 'Base.User'
 # CORS
 # ------------------------
 CORS_ALLOWED_ORIGINS = [
+    "http://102.164.134.4:8000",
     "http://localhost:3000",  # l'URL de ton frontend React
 ]
 
