5410270adf3beaf4194ffaf6c72b171af5960f7c
Amadiane <129948408+Amadiane@users.noreply.github.com>
2025-09-04 13:05:58 +0100

    hh

--- Server/transfert/settings.py
+++ Server/transfert/settings.py
@@ -10,7 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ------------------------
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 DEBUG = True
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "102.164.134.4"]
+ALLOWED_HOSTS = ["diallodiallotransfert.com", "www.diallodiallotransfert.com", "102.164.134.4"]
+# ALLOWED_HOSTS = ["127.0.0.1", "localhost", "102.164.134.4"]
 
 
 AUTH_USER_MODEL = 'Base.User'
@@ -19,13 +20,16 @@ AUTH_USER_MODEL = 'Base.User'
 # CORS
 # ------------------------
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # l'URL de ton frontend React
-    "http://102.164.134.4:8000",
+    # "http://localhost:3000",  # l'URL de ton frontend React
+    # "http://102.164.134.4:8000",
+    "http://diallodiallotransfert.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost:3000",
-    "http://102.164.134.4:3000",
+    #"http://localhost:3000",
+    #"http://102.164.134.4:3000",
+    "http://diallodiallotransfert.com",
+    #"https://diallodiallotransfert.com",
 ]
 
 # ------------------------
